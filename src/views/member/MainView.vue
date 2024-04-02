d0e0ea24818af05b5f5922b9d537c89023200f1a
poula9744 <poula9744@naver.com>
2024-04-03 01:42:17 +0900

    메인페이지

--- src/views/member/MainView.vue
+++ src/views/member/MainView.vue
@@ -2,7 +2,7 @@
     <div id="back">
         <div id="back2">
             <div>
-                <h1 id="fir"><strong class="na">김수빈</strong> 회원님,</h1>
+                <h1 id="fir"><strong class="na">{{ memberVo.name }}</strong> 회원님,</h1>
                 <h1 id="sec">안녕하세요!</h1>
             </div>
             <div id="oooo">
@@ -11,19 +11,22 @@
                         <tbody>
                             <tr>
                                 <td id="tttt">나의 등록일</td>
-                                <td id="yyyy">2024-00-00</td>
+                                <td id="yyyy">{{ memberVo.regDate }}</td>
                             </tr>
                             <tr>
                                 <td id="tttt">회원권 만료일</td>
-                                <td id="yyyy">2024-00-00</td>
+                                <td id="yyyy">{{ memberVo.deadline }}</td>
                             </tr>
                             <tr>
                                 <td id="tttt">남은 PT횟수</td>
-                                <td id="yyyy" class="yyyt">0회 <button id="tkdtp">상세</button></td>
+                                <td id="yyyy" class="yyyt">
+                                    {{memberVo.ptCount}}회
+                                    <button id="tkdtp">상세</button>
+                                </td>
                             </tr>
                             <tr>
                                 <td id="tttt">나의 강사님</td>
-                                <td id="yyyy">강사님이 없습니다</td>
+                                <td id="yyyy">{{ memberVo.trainerName }} 강사님</td>
                             </tr>
                         </tbody>
                     </table>
@@ -55,7 +58,7 @@
                     id="box34">나의<br>회원</button>
             </div>
             <div id="boxbox">
-                <button id="box37">락커 번호 : 000번</button>
+                <button id="box37">락커 번호 : {{memberVo.lockerNo}}번</button>
             </div>
 
         </div>
@@ -68,11 +71,55 @@
 
 </template>
 <script>
+import axios from 'axios';
 export default {
-    name: 'LoginClear',
+    emits: ['point'],
+    name: 'memberMainView',
     components: {},
     data() {
-
+        return {
+        memberVo: {
+            name: "",
+            regDate: "",
+            deadline: "",
+            ptCount: "",
+            trainerName: "",
+            lockerNo: ""
+        },
+        attendVo: {
+            leaveTime: "",
+            entryTime: ""
+        }
+        };
+    },
+    methods: {
+        mainform() {
+        console.log("mainform");
+        axios({
+            method: "get",
+            url: "http://localhost:9000/api/member/main",
+            headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            Authorization: "Bearer " + this.$store.state.token
+            },
+            responseType: "json"
+        }).then(response => {
+            console.log(response.data);
+            if (response.data.result == "success") {
+            console.log("result: success");
+            this.memberVo = response.data.apiData;
+            } else {
+            console.log(response.data.message);
+            alert("로그인 하세요");
+            this.$router.push("/member/login");
+            }
+        }).catch(error => {
+            console.log(error);
+        });
+        }
+    },
+    created() {
+        this.mainform();
     }
-}
+};
 </script>
--- src/views/trainer/MainView.vue
+++ src/views/trainer/MainView.vue
@@ -2,31 +2,34 @@
     <div id="back">
         <div id="back2">
             <div>
-                <h1 id="fir"><strong class="na">박종희</strong> 강사님,</h1>
+                <h1 id="fir"><strong class="na">{{ trainerVo.name }}</strong> 강사님,</h1>
                 <h1 id="sec">안녕하세요!</h1>
             </div>
             <div>
-                <img id="pi" src="" alt="">
+                <img id="pi" v-bind:src="`http://localhost:9000/upload/{{ trainerVo.saveName }}`">
             </div>
             <div>
                 <table id="count">
                     <tbody>
                         <tr>
                             <td id="allcount">전체 회원수</td>
-                            <td id="sercount">200명</td>
+                            <td id="sercount">{{ trainerVo.totalMember }} 명</td>
                         </tr>
                         <tr>
                             <td id="allcount">나의 회원수</td>
-                            <td id="sercount">?명</td>
+                            <td id="sercount">{{ trainerVo.trainerMember }}명</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
             <div>
-                <button id="box33">트레이너<br>등록</button><button id="box33">전체<br>회원</button><button id="box34">나의<br>회원</button>
+                <button id="box33">트레이너<br>등록</button>
+                <button id="box33">전체<br>회원</button>
+                <button id="box34">나의<br>회원</button>
             </div>
             <div id="boxbox">
-                <button id="box35">회원정보 수정</button><button id="box36">LOG OUT</button>
+                <button id="box35">회원정보 수정</button>
+                <button id="box36" v-on:click.prevent="logout">LOG OUT</button>
             </div>
         </div>
         <div>
@@ -41,12 +44,59 @@
 
 </template>
 <script>
+import axios from 'axios';
 import "@/assets/css/Main.css"
 export default{
-    name: 'LoginClear',
+    name: 'MainView',
     components: {},
     data(){
+        return{
+            trainerVo: {
+            name: "",
+            saveName: "",
+            totalMember: "",
+            trainerMember: ""
+        }
+        };
+    },
+    methods: {
+        mainform(){
+        console.log("mainform");
+        axios({
+            method: 'get',  //put,post,delete
+            url: 'http://localhost:9000/api/trainer/main',
+            headers: { "Content-Type": "application/json; charset=utf-8" 
+                        , "Authorization": "Bearer " + this.$store.state.token
+            }, //전송타입 + 토큰
 
+            //params: userVo, //get방식 파라미터로 값이 전달
+            //data: this.userVo.no, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+
+            responseType: 'json' //수신타입
+        }).then(response => {
+            console.log(response.data); //수신데이타
+            if(response.data.result == "success"){
+                console.log("result: success");
+                this.trainerVo = response.data.apiData;
+            } else {
+                console.log(response.data.message);
+                alert("로그인 하세요");
+                this.$router.push("/trainer/login");
+            }
+
+        }).catch(error => {
+            console.log(error);
+        });
+    },
+        logout(){
+                console.log("로그아웃");
+                this.$store.commit("setAuthUser", null);
+                this.$store.commit("setToken", null);
+                this.$router.push("/trainer/login");
+        }
+    },
+    created(){ 
+        this.mainform();
     }
 }
 </script>
